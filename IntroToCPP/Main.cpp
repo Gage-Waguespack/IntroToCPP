3999affa8efedd899f980040a4207a8e96dcb088
Gage <gagewaguespack@gmail.com>
2020-12-07 16:42:05 -0600

    Added more a way for the player to make choices and broke the game
    Game broke with the mid function.

--- IntroToCPP/Main.cpp
+++ IntroToCPP/Main.cpp
@@ -2,20 +2,63 @@
 
 int main()
 {
+    char answer;
+    int num;
+    int min;
+    int max;
 
     std::cout << "Pick a number between one and one hundred" << std::endl;
 
     system("pause");
 
+    std::cout << "Rules: Answer all questions with either Y for yes or N for no!" << std::endl;
+
+
+    std::cout << "You must also answer G for greater than or L for less than!" << std::endl;
+
+
     std::cout << "Remember that number and DO NOT CHANGE IT!" << std::endl;
 
     system("pause");
+    system("CLS");
 
-    int num = 0;
+    std::cout << "Now, Let's get started! Is your number 50?" << std::endl;
 
-    std::cin >> num;
+    int num = 50;
+    int min = 0;
+    int max = 100;
 
+    void mid(int min, int max);
+    {
+        (min + max) / 2;
+    }
 
+    std::cin >> answer;
+
+    if (answer != 'Y' && answer != 'N' && answer != 'G' && answer != 'L')
+    {
+        if (answer == 'Y')
+        {
+            std::cout << "Your number is " << num << "!" << std::endl;
+
+            system("pause");
+
+            return 1;
+        }
+
+        else if (answer == 'N')
+        {
+            std::cout << "Is the number greater or less than 50?" << std::endl;
+
+            system("pause");
+
+            if (answer == 'G')
+            {
+                mid(51, 100);
+            }
+        }
+
+    }
     
     system("pause");
     return 1;
