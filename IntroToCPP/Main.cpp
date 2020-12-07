f0ee083fde19e5b1223919cc9c7a920aa6529b1d
Gage <gagewaguespack@gmail.com>
2020-12-07 14:59:21 -0600

    Created new vcxproj inside of the main folder

---
+++ IntroToCPP/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Pick a number between one and one hundred" << std::endl;
+
+    system("pause");
+
+    std::cout << "Remember that number and DO NOT CHANGE IT!" << std::endl;
+
+    system("pause");
+
+    int num = 0;
+
+    std::cin >> num;
+
+
+    
+    system("pause");
+    return 1;
+}
