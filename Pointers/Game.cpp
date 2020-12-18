2bb0896c225a594b6ac97d10d578b0b8b2ea9a59
Gage <gagewaguespack@gmail.com>
2020-12-18 11:57:03 -0600

    Added a way to save and load as well as started to implement it in game!

--- Pointers/Game.cpp
+++ Pointers/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <iostream>
+#include <fstream>
 
 void Game::run()
 {
@@ -22,13 +23,15 @@ void Game::start()
 	m_player2 = new Character(100, 10);
     bool gameOver = false;
     int input;
+    //Create an instance of a file stream.
+    std::fstream file;
 
     ///This is an array that holds characters to create a name for player one
     char nameOne[15];
     char playerSymbol;
     ///This should check if player one wins
-    bool checkPlayer1Win = false;
-    if (checkPlayer1Win == true)
+    bool player1Win = false;
+    if (player1Win == true)
     {
         gameOver = true;
     }
@@ -40,8 +43,8 @@ void Game::start()
     ///This is an array that holds characters to create a name for player two
     char nameTwo[15];
     ///This should check if player two wins
-    bool checkPlayer2Win = false;
-    if (checkPlayer2Win == true)
+    bool player2Win = false;
+    if (player2Win == true)
     {
         gameOver = true;
     }
@@ -57,15 +60,110 @@ void Game::start()
     system("pause");
     system("cls");
 
-    while (gameOver == false)
+    std::cout << nameOne << " would you like to load a save?" << std::endl;
+    std::cout << "1. Yes" << std::endl;
+    std::cout << "2. No" << std::endl;
+    std::cin >> input;
+    if (input == 1)
     {
-        std::cout << "It is now your turn " << nameOne << ", what would you like to do?" << std::endl;
-        std::cout << "1. Attack" << "2. Leave" << std::endl;
-        std::cin >> input;
-        if (input == 1)
+        //Search for a file that has the name given in the first
+        //argument. The"std::ios::in" argument is used to say
+        // that we want to get input from the file.
+        file.open("save1.txt", std::ios::in);
+
+        //Check if the file isn't opened. If so return from main.
+        if (!file.is_open())
         {
-            
+            std::cout << "Could not load save data!" << std::endl;
         }
+
+        //Reads the player stats from the text file.
+        file >> m_player1->getHealth();
+        file >> m_player1->getDamage();
+
+        //Closes the file once we're done.
+        file.close();
+
+        std::cout << m_player2->getHealth() << std::endl;
+        std::cout << m_player2->getDamage() << std::endl;
+
+    }
+    if (input == 2)
+    {
+        std::cout << "You've chosen not to load a save!" << std::endl;
+    }
+
+    std::cout << "Now, you have 10 rounds to take out the other player!" << std::endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        while (gameOver == false)
+        {
+            std::cout << "It is now your turn " << nameOne << ", what would you like to do?" << std::endl;
+            std::cout << "1. Attack" << std::endl;
+            std::cout << "2. Leave" << std::endl;
+            std::cout << "3. Save" << std::endl;
+            std::cin >> input;
+            if (input == 1)
+            {
+                std::cout << nameOne << " attacked " << nameTwo << "!!" << std::endl;
+                m_player1->attack(m_player2);
+                std::cout << nameTwo << "lost " << m_player1->getDamage() << "!!" << std::endl;
+
+            }
+            if (input == 2)
+            {
+                std::cout << nameOne << " has left the fight!" << std::endl;
+                std::cout << nameTwo << " is the winner by forfeit!" << std::endl;
+                player2Win == true;
+            }
+            if (input == 3)
+            {
+                //Search for the file.
+                file.open("save1.txt", std::ios::out);
+
+                //Check if the file isn't opened. If so return.
+                if (!file.is_open())
+                {
+                    std::cout << "No save was found!" << std::endl;
+                    break;
+                }
+
+                //Write player stats to the text file.
+                file << m_player1->getHealth() << std::endl;
+                file << m_player1->getDamage() << std::endl;
+
+                //Close the file when we're done.
+                file.close();
+            }
+
+            std::cout << "It is now your turn " << nameTwo << ", what would you like to do?" << std::endl;
+            std::cout << "1. Attack" << std::endl;
+            std::cout << "2. Leave" << std::endl;
+            std::cout << "3. Save" << std::endl;
+            std::cin >> input;
+            if (input == 1)
+            {
+                std::cout << nameTwo << " attacked " << nameOne << "!!" << std::endl;
+                m_player2->attack(m_player1);
+                std::cout << nameOne << "lost " << m_player2->getDamage() << "!!" << std::endl;
+
+            }
+            if (input == 2)
+            {
+                std::cout << nameTwo << " has left the fight!" << std::endl;
+                std::cout << nameOne << " is the winner by forfeit!" << std::endl;
+                player1Win == true;
+            }
+
+            if (input == 3)
+            {
+
+            }
+
+        }
+
 
     }
 
