ecf0a78a04321a02a5b2acd13c95bbe85f5a8078
Gage <gagewaguespack@gmail.com>
2021-01-11 11:33:25 -0600

    added Comments for the number guessing game

--- IntroToCPP/Main.cpp
+++ IntroToCPP/Main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-
+	//Tells the player the rules
     std::cout << "Pick a number between one and ten" << std::endl;
 
     system("pause");
@@ -13,13 +13,16 @@ int main()
 
     std::cout << "Remember that number and DO NOT CHANGE IT!" << std::endl;
 
+	//Pauses before starting
     system("pause");
     system("CLS");
 
+	//Sets a min and max value, as well as a length
     int length = 10;
     int min = 1;
     int max = 10;
 
+	//Loops the main game while the min is less than or equal to the max
 	while (min <= max)
 	{
 		int num = 0;
@@ -28,17 +31,17 @@ int main()
 		bool correct = 3;
 		int guess = (min + max) / 2;
 
+		//Displays a number on screen
 		std::cout << "Is this your number? " << guess << std::endl;
 
+		//Asks if the number is higher, lower, or correct
 		std::cout << "1. Higher" << std::endl;
 		std::cout << "2. Lower" << std::endl;
 		std::cout << "3. Correct" << std::endl;
 
-
-
 		std::cin >> num;
 
-
+		//Checks if the player cheated by changing their number
 		if (num == 1)
 		{
 			if (min == max)
@@ -48,6 +51,7 @@ int main()
 			min = guess + 1;
 
 		}
+		//Checks again if the player cheated by changing their number
 		else if (num == 2)
 		{
 			if (min == max)
@@ -57,6 +61,7 @@ int main()
 			max = guess - 1;
 
 		}
+		//Checks if the number is correct, if so, the player wins
 		else if (num == 3)
 		{
 			std::cout << "Told You I would get it" << std::endl;
